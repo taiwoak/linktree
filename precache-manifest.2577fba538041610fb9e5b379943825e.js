a37680d92ef6d21754dc3efbd10538b34c1d3873
Taiwo Akerele <taiwoakerele98@gmail.com>
2022-10-31 07:19:56 +0100

    my linktree clone react app

---
+++ precache-manifest.2577fba538041610fb9e5b379943825e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d48dcf6fea69a1de25",
+    "url": "/linktree/static/css/main.b16dc611.chunk.css"
+  },
+  {
+    "revision": "f5d48dcf6fea69a1de25",
+    "url": "/linktree/static/js/main.f5d48dcf.chunk.js"
+  },
+  {
+    "revision": "67ca7bb7b3f4d8b79922",
+    "url": "/linktree/static/css/1.bcf87122.chunk.css"
+  },
+  {
+    "revision": "67ca7bb7b3f4d8b79922",
+    "url": "/linktree/static/js/1.67ca7bb7.chunk.js"
+  },
+  {
+    "revision": "fbc58bb9004eecdf25c1",
+    "url": "/linktree/static/js/runtime~main.fbc58bb9.js"
+  },
+  {
+    "revision": "2c986b411d0c83a629a19a7490fdc682",
+    "url": "/linktree/index.html"
+  }
+];
